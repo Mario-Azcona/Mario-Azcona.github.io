40c50756f70dc7306c61e71be1c5dffd9a4f40a9
Mario-Azcona <mazconaaizcorbe@gmail.com>
2022-05-13 13:32:12 +0200

    Update buttons

--- buttonsClick.js
+++ buttonsClick.js
@@ -3,19 +3,15 @@ const playButton = document.querySelector('#playButton');
 const pauseButton = document.querySelector('#pauseButton');
 
 playButton.addEventListener("click", event => {
-    playButton.setAttribute("class","");
-    playButton.setAttribute("visible","false");
+    playButton.setAttribute("visible",false);
 
-    pauseButton.setAttribute("class","clickable");
-    pauseButton.setAttribute("visible","true");
+    pauseButton.setAttribute("visible",true);
 
 });
 
 pauseButton.addEventListener("click", event => {
-    pauseButton.setAttribute("class","");
-    pauseButton.setAttribute("visible","false");
+    pauseButton.setAttribute("visible",false);
 
-    playButton.setAttribute("class","clickable");
-    playButton.setAttribute("visible","true");
+    playButton.setAttribute("visible",true);
 
 });
